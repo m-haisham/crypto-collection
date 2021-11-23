2a0688a8f3ca04332df8803c1c7a24541a1e4e8e
Mensch272 <47662901+mensch272@users.noreply.github.com>
2021-11-23 22:17:32 +0500

    Adapted to adminkit ui

--- crypto/views.py
+++ crypto/views.py
@@ -8,28 +8,12 @@ Contributor = namedtuple('Contributor', 'id name alias description profile thumb
 
 contributors = [
     Contributor(
-        id='S1901061',
-        name='Haisham',
-        alias='Mensch272',
-        description='I am a human 🤥.',
-        profile='https://github.com/mensch272',
-        thumbnail='https://avatars.githubusercontent.com/u/47662901?v=4',
-    ),
-    Contributor(
         id='S1900884',
         name='Maadh',
         alias='maadhmohamed',
         description='</>.',
         profile='https://github.com/maadhmohamed',
         thumbnail='https://avatars.githubusercontent.com/u/44810382?v=4',
-    ),
-    Contributor(
-        id='S1801024',
-        name='Sahaf',
-        alias='Codemachine0101',
-        description='Connoisseur of Tamamo.',
-        profile='https://github.com/Codemachine0101',
-        thumbnail='https://avatars.githubusercontent.com/u/53483283?v=4',
     ),
 ]
 
